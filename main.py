5e41398b4d7d76ff9d6edeb0b95e583e4aac1be0
creanero <creanero@gmail.com>
2021-12-09 17:31:32 +0000

    demo of two files read

--- main.py
+++ main.py
@@ -6,42 +6,65 @@ import json
 from datetime import datetime
 import pandas as pd
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # Creates the dictionary to hold the pulses
-    pulse_data = {}
-
-    # Creates header data
-    pulse_data['name'] = 'Demo pulses'
-    pulse_data['purpose'] = 'This is a demo pulse to show how JSON files work'
-
-    # Creates a list to hold the pulses inside the dictionary
-    pulse_data['pulses'] = []
-
-    # creates a variable for a pulse label
-    next_pulse=12345
-
+def read_pulse_from_csv(pulse_data, csv_file, pulseID):
     # reads in pulse data from a file
-    IQ_data = pd.read_csv('pulse_short.csv').to_dict(orient='list')
-
+    IQ_data = pd.read_csv(csv_file).to_dict(orient='list')
     # creates a timestamp for the pulse
     # (this should probably not be done at write time, but is here for demo purposes - OC)
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    timestamp_format = '%Y%m%d%H%M%S%f'
+    timestamp = datetime.now().strftime(timestamp_format)
 
     # appends the demo pulse to the list of pulses
     pulse_data['pulses'].append(
         {
-        'pulseID': next_pulse,
+        'pulseID': pulseID,
         'timestamp': timestamp,
+        'timestamp_format': timestamp_format,
         'IQ_data': IQ_data
         })
 
-    # Increments the pulse label
-    next_pulse+=1
+def main():
+    # Creates the dictionary to hold the pulses
+    pulse_data = {}
 
+    # Creates a list to hold the pulses inside the dictionary
+    # put this first due to the LIFO way JSON is written
+    pulse_data['pulses'] = []
+
+    # Creates a timestamp for the overall data
+    timestamp_format = '%Y%m%d%H%M%S'
+    timestamp = datetime.now().strftime(timestamp_format)
+    pulse_data['timestamp'] = timestamp
+    pulse_data['timestamp_format'] = timestamp_format
+
+    # Creates header data
+    pulse_data['purpose'] = 'This is a demo pulse to show how JSON files work'
+    pulse_data['name'] = 'Demo pulses'
+
+    # creates a variable for a pulse label
+    next_pulse=12345
+
+    for i in [1,2]:
+        # reads in the data from the csv file
+        csv_file = "pulse_short_"+str(i)+".csv"
+        read_pulse_from_csv(pulse_data, csv_file, i)
+
+    # # reads in the data from the csv file
+    # csv_file = "pulse_short_2.csv"
+    # read_pulse_from_csv(pulse_data, csv_file, next_pulse)
+    #
+    # # Increments the pulse label
+    # next_pulse+=1
 
     # Writes the dictionary to a file
     # (Using the timestamp from above to create unique filenames. Should probably use something better in production)-OC
     with open('data'+timestamp+'.json', 'w') as outfile:
         json.dump(pulse_data, outfile)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    main()
+
+
 
